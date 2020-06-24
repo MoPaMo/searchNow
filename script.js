2db50c845739b736fa93ef1d01b45b962d5fe41f
Glitch (hello-webpage) <none>
2020-06-24 12:00:34 +0000

    ☂️🏆 Checkpoint ./engines.json:11290180/154 ./script.js:11290180/1768 ./index.html:11290180/189

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+$(document).ready(function(){
+  $("button").click(function(){
+    $.ajax({url: "demo_test.txt", success: function(result){
+      $("#div1").html(result);
+    }});
+  });
+});
