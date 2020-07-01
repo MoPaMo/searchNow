e71910e5618b2f91c4ff7aeb1f3b59b12cb73d2c
Glitch (hello-webpage) <none>
2020-07-01 07:50:21 +0000

    🚘💂 Checkpoint ./script.js:11290180/1757 ./engines.json:11290180/4293 ./index.html:11290180/8562

--- script.js
+++ script.js
@@ -4,10 +4,7 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-
-
-
- /* var xhttp = new XMLHttpRequest();
+/* var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var engines= this.responseText;
@@ -17,17 +14,16 @@ console.log("hi");
   xhttp.open("GET", "style.css", true);
   xhttp.send();
 */
-$(document).ready(function(){
-  
-    $.getJSON("/engines.json", function(result){
-      
-      $.each(result.categories, function(i, field){
-        $("#out").append(" <h4 class=\"lead\">"+field.name+"</h4>");
-        $.each(field, function(i2, field2){
-        $("#out").append("<a href=\""+field2.url+"\">"+field2.name + " ");
+$(document).ready(function() {
+  $.getJSON("/engines.json", function(result) {
+    $.each(result.categories, function(i, field) {
+      $("#out").append(
+        ' <h4 class="lead">' + i+ "</h4>"
+      );
+      $.each(field, function(i2, field2) {
+        $("#out").append('<a href="' + field2.url + '">' + field2.name + " ");
       });
-      });
-      $("#loading").remove();
+    });
+    $("#loading").remove();
   });
 });
-
