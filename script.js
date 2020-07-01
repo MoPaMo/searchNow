501a3a565d360a6f223c1d28ac3632e9de185078
Glitch (hello-webpage) <none>
2020-07-01 08:46:36 +0000

    🏹🎫 Checkpoint ./engines.json:11290180/78 ./index.html:11290180/30 ./script.js:11290180/2835

--- script.js
+++ script.js
@@ -17,13 +17,22 @@ console.log("hi");
 $(document).ready(function() {
   $.getJSON("/engines.json", function(result) {
     $.each(result.categories, function(i, field) {
-      $("#out").append(
-        ' <h4 class="lead">' + i+ "</h4>"
-      );
+      $("#out").append(' <h4 class="lead">' + i + "</h4>");
       $.each(field, function(i2, field2) {
-        $("#out").append('<a href="' + field2.url + '">' + field2.name + " ");
+        $("#out").append(
+          "<button onclick=\"op('" +
+            field2.url +
+            ' \')" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" data-original-title="' +
+            field2.plus +
+            '">' +
+            field2.name +
+            " </button>"
+        );
       });
     });
     $("#loading").remove();
   });
 });
+//<button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
+// Tooltip on right <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip on bottom">Tooltip on bottom</button>
+//</button>
